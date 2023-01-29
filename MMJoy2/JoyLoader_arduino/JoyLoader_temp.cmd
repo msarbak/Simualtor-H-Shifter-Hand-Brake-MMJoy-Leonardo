6d11a0df7b43f151afff05b936635033a6140533
msarbak <m.sarbak@hotmail.com>
2023-01-29 13:08:56 +0300

    first commit

---
+++ MMJoy2/JoyLoader_arduino/JoyLoader_temp.cmd
@@ -0,0 +1,2 @@
+@avrdude.exe -C avrdude.conf -c avr109 -P COM3 -D -V -p ATmega32U4 -U flash:w:C:\Users\mehmetsarbak\Desktop\MMJoy2\Firmware\Firmware_lufa_[MMJOY2.ATMEGA32U4].hex:i
+@ping 127.0.0.1 >> null
